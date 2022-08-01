53bba21fad3aeb52ef58c3c64a6ffb410dc6f713
Sayyora2021 <96926176+Sayyora2021@users.noreply.github.com>
2022-08-01 18:06:29 +0200

    product_description&name

--- front/js/product.js
+++ front/js/product.js
@@ -1,67 +1,31 @@
-/*then(function(value) {
-  // sessionStorage.setItem("data", JSON.stringify(value))
-   displayItems(value)
-   console.log(value)
-   })*/
+//pour recuperer l'id existant
 const idProduct = new URL(document.location).searchParams.get('id');
-const item_img = document.querySelector(".item_img");
-
+const itemImg = document.querySelector('.item__img');
+const kanapName = document.querySelector('#title');
+//const itemContent = document.querySelector('.item__content');
 
 //pour recuperer le produit
 fetch(`http://localhost:3000/api/products/${idProduct}`)
-.then((response) =>response.json())
-.then((product) =>{
-const itemImg = document.querySelector(".item_img");
+  .then((response) => response.json())
+  .then((product) =>{
+    const imageKanap = document.createElement('img');
+    imageKanap.src = product.imageUrl;
+    imageKanap.alt = product.altTxt;
+    itemImg.appendChild(imageKanap);
+
+    let kanapName = document.querySelector ('#title');
+    kanapName.textContent = (`${product.name}`);
+
+    let kanapPrice = document.querySelector('#price');
+    kanapPrice.texteContent = (`${product.price}`);
+    
+    let kanapDescription = document.querySelector('#description');
+    kanapDescription.textContent = (`${product.description}`);
+
+    
+
+ })
+
  
 
-const imageKanap = document.createElement("img");
-imageKanap.src = product.imageUrl;
-imageKanap.alt= product.altTxt;
-itemImg.appendChild(imageKanap);
-})
 
-//const displayProducts = (product)=> {
-  
-
-
-/*image = document.createElement("img");
-image.src =product.imageUrl;
-image.alt = product.altTxt;
-
-//recuperation des elements existants dans HTML
-/*const produitImage = document.querySelector(".item__img");
-console.log(produitImage)
-
-
-
-/*const produitPrice = document.querySelector(".item__content__titlePrice");
-document.querySelector("item__content").appendChild("item__content__titlePrice")
-console.log(produitPrice)
-
-/*function handleData (canap1){
-  const {altTxt, colors, description, imageUrl, name, price, _id} = canap1
-}
-
-
-
-//titre = document.createElement()
-
-/*titre= document.createElement("h1");
-titre.textContent = product.name;
-items.appendChild (titre);
-
-
-if(search_params.has('name')) {
-  var name = search_params.get('name');
-  console.log(name)
-}sole.log(name);*/
-//solution
-/*const url = new URL(window.location.href);
-const urlParams = new URLSearchParams(url.search);
-console.log(urlParams);
-
-let id = "";
-if (urlParams.has("id")) {
-  id = urlParams.get("id");
-  console.log(id);
-}*/
--- front/js/script.js
+++ front/js/script.js
@@ -2,20 +2,19 @@
 const itemsTag = document.getElementById("items")
 
 // la fonction qui va récupérer les données de l'API
-//const getData = function(){
 fetch("http://localhost:3000/api/products")
 //récupération des données de lAPI dans response.json
 .then((response) => response.json())
 .then(function(value) {
-// sessionStorage.setItem("data", JSON.stringify(value))
+
  displayItems(value)
  console.log(value)
  })
-//}
+
 
 // appel de la fonction d'affichage
 const displayItems = function (value){
-// items.innerHTML = ""
+
  for (let product of value){
    let a = document.createElement("a");
 a.href = `./product.html?id=${product._id}`
@@ -51,67 +50,3 @@ article.appendChild(p);
   <h3 class='productName'>${product.name}</h3>
   <p class="productDescription">${product.description}</p>
 </article></a>`);*/
-/*titre= document.createElement("h3");
-titre.textContent = product.name;
-article.appendChild (titre);
-
-text = document.createElement("p");
-text.productDescription = product.decsription;
-
-image = document.createElement("img");
-image.src =product.imageUrl;
-article = document.createElement("article");
-article.appendChild(image);
-article.appendChild(titre);
-
-items.appendChild(article);
- 
-/*if(sessionStorage.getItem("data") !== null){
-const dataSession = JSON.parse(sessionStorage.getItem("data"));
-console.log(dataSession)
-displayItems(dataSession)
-}
-else{
- getData();
-}*/
-
- 
-
- /*function makeAnchor(id){
-// création d'un element  
- const anchor = document.createElement("a")
- anchor.href = "./product.html?id=" + id
- return anchor
- }
-
- // on recupere la balise section
- const sectionId = document.getElementById('items')
-console.log(sectionId)
-
- function appendChildren(anchor){
-const items = document.querySelector("#items")
-if (items != null){
- items.appendChild(anchor)
-}
-}
-// On creer la balise a
-let a = document.createElement('a')
-// on creer l'attribut de la balise
-a.setAttribute('href','./product.html?id=42')
-// parent.appendChild('enfant')
-sectionId.appendChild(a)
-
-
-// On recupere les données dans un array
-// On parcour se tableau
-// Pour chaque produit
-// on creer les balises et attribut adéquat
-
-//InnertHtml
-/*parent.innertHtml =  <a href="./product.html?id=42">
-            <article>
-            <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1"></img>
-            <h3 class="productName">Kanap name1</h3>
-            <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-          </article>
-        </a> */
